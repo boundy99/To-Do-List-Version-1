2b932afc2dcb43f9547753b3863eaf05d982aa2c
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-18 18:22:14 -0500

    Post request

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    if (currentDay == 6 || currentDay == 0) {
-        day = "Weekend"
-    } else day = "Weekday"
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
 
+    var day = today.toLocaleDateString("en-US", options)
     res.render('list', { kindOfDay: day });
+})
+
+app.post('/', (req, res) => {
+    var newItem = req.body.toDo;
+    console.log(newItem)
 })
 
 app.listen(3000, () => {
