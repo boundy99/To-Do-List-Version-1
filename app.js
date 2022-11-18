d8dae8c84d2f71da5a7ca0083529a166bbfb5c25
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-18 18:55:19 -0500

    Styling

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+let newItems = ['Buy Food', 'Cook Food', 'Eat Food'];
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     var today = new Date();
 
-    var options = {
+    let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
     };
 
-    var day = today.toLocaleDateString("en-US", options)
-    res.render('list', { kindOfDay: day });
+    let day = today.toLocaleDateString("en-US", options)
+    res.render('list', { kindOfDay: day, newElement: newItems });
 })
 
 app.post('/', (req, res) => {
-    var newItem = req.body.toDo;
-    console.log(newItem)
+    let newItem = req.body.toDo;
+    newItems.push(newItem);
+    res.redirect('/');
 })
 
 app.listen(3000, () => {
