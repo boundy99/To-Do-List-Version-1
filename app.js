e5a002690d5fcd14408563583a9f8e0c24a6b6e8
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-11-18 20:04:30 -0500

    EJS multiple pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 let newItems = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 
 const app = express();
 
@@ -21,13 +22,33 @@ app.get('/', (req, res) => {
     };
 
     let day = today.toLocaleDateString("en-US", options)
-    res.render('list', { kindOfDay: day, newElement: newItems });
+    res.render('list', { listTitle: day, newElement: newItems });
 })
 
 app.post('/', (req, res) => {
+    console.log(req.body)
+
     let newItem = req.body.toDo;
+    if (req.body.list === "Work") {
+        workItems.push(newItem);
+        res.redirect('/work')
+    }
     newItems.push(newItem);
     res.redirect('/');
+})
+
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: "Work List", newElement: workItems })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.post('/work', (req, res) => {
+    let workItem = req.body.toDo;
+    workItems.push(workItem);
+    res.redirect('/work');
 })
 
 app.listen(3000, () => {
